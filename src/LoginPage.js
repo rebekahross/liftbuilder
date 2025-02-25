dea51cabf79609255d3ebe92be75c4eff1e9614b
Rebekah Ross <rebekahross123@gmail.com>
2025-02-25 09:46:03 -0700

    initial commit

---
+++ src/LoginPage.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import styles from "./loginPage.module.scss";
+
+const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    if (email === "admin@example.com" && password === "password") {
+      alert("Login successful!");
+      navigate("/");
+    } else {
+      alert("Invalid credentials");
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleLogin} className={styles.formWrapper}>
+        <div className={styles.loginContainer}>
+          <h1 className={styles.logoText}>LiftBuilder</h1>
+          <div>
+            <input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              className={styles.inputField}
+            />
+          </div>
+          <div>
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              className={styles.inputField}
+            />
+          </div>
+          <button type="submit" className={styles.loginButton}>
+            Login
+          </button>
+          <div>
+            Don't have an account?{" "}
+            <a className={styles.signupLink} href="/signup">
+              Sign up!
+            </a>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
